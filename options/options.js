26c70e3e54262b1803f024e73075f3463d826f58
Crowdbotics <engineering@crowdbotics.com>
2023-04-11 18:14:47 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tiwariapp-dev-75980",
+  url: "https://tiwariapp-dev-75980.botics.co",
+  api: "https://tiwariapp-dev-75980.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
